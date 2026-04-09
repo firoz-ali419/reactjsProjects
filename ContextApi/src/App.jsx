eb70739ad6cf486bb12813a23529f47e5b3475ee
FirozKhanHuessain <fzkhan9536@gmail.com>
2026-04-09 10:20:05 +0530

    add full functionality for contextApi

--- ContextApi/src/App.jsx
+++ ContextApi/src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import { todoProvider } from './context'
 
 
 function App() {
+  const [todos,setTodos]=useState([])
 
+  const addTodo=(todo)=>{
+    setTodos((prevTodo)=>[{id:Date.now(),...todo},...prevTodo])
+  }
+
+  const updateTodo=(id,todo)=>{
+    setTodos((prevTodo)=>prevTodo.map((prev)=>prev.id===id?todo:prev))
+  }
+
+  const deleteTodo=(id)=>{
+    setTodos((prevTodo)=>prevTodo.filter((prev)=>prev.id!==id))
+  }
+  const toggleComplete=(id)=>{
+    setTodos((prevTodo)=>prevTodo.map((prev)=>prev.id===id?[{...prev,complete:!prev.complete}]:prev))
+  }
+
+  useEffect(()=>{
+    const todos=JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length>0)
+      setTodos(todos)
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
 
   return (
-    <>
+    <todoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
      <h1>Hello World!!</h1>
-    </>
+    </todoProvider>
   )
 }
 
---
+++ ContextApi/src/context/index.js
@@ -0,0 +1,1 @@
+export {todoProvider,todoContext,useTodo} from "./TodoContext"
